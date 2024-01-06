f7aba34f5046282a3742b5f1a54b2b9ca919c7c9
andrewijaya <andrewijaya988@gmail.com>
2024-01-06 12:08:42 +0700

    refactor: add icon and remove some text

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -29,15 +29,14 @@ export default function Service() {
     <section id="services" className="">
       <div>
         <h3 className="text-3xl font-medium text-teal-500  dark:text-teal-400 mb-2">
-          Services I offer
+          Services I offer ✍️
         </h3>
         <p className="py-2 text-gray-800 dark:text-white">
-          Welcome to my Full Stack Development service! I specialize in
-          seamlessly integrating front-end and back-end technologies to create
-          dynamic web applications. Whether it&apos;s designing user-friendly
-          interfaces or implementing robust server-side functionalities, I
-          deliver efficient and scalable solutions. Let&apos;s transform your
-          ideas into a digital reality!
+          I specialize in integrating front-end and back-end technologies to
+          create dynamic web applications. Whether it&apos;s designing
+          user-friendly interfaces or implementing robust server-side
+          functionalities, I deliver efficient and scalable solutions.
+          Let&apos;s transform your ideas into a digital reality!
         </p>
       </div>
       <div className="md:flex max-w-full gap-10 dark:text-slate-300">
