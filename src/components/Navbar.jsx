c16b503b073866ad5738889f0693e1e9f45a97dc
andrewijaya <andrewijaya988@gmail.com>
2024-01-06 14:40:43 +0700

    style: make more gud

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,36 +10,41 @@ Navbar.propTypes = {
 
 export default function Navbar({ darkMode, setDarkMode }) {
   return (
-    <nav className="py-10 flex items-center justify-between relative font-semibold dark:text-white">
-      <h1 className=" z-10 text-xl md:text-2xl">Andre</h1>
+    <nav className="py-10 flex justify-between relative font-semibold dark:text-white">
+      {/* <h1 className=" z-10 text-xl md:text-2xl">Andre</h1> */}
 
+      <h1 className="">
+        {darkMode ? (
+          <FaSun
+            className="cursor-pointer text-2xl fill-yellow-300"
+            onClick={() => setDarkMode(!darkMode)}
+          />
+        ) : (
+          <BsMoonStarsFill
+            className="cursor-pointer text-2xl  "
+            onClick={() => setDarkMode(!darkMode)}
+          />
+        )}
+      </h1>
       <ul className="flex items-center w-auto md:py-0 py-4 gap-x-4">
-        <li className="">
-          {darkMode ? (
-            <FaSun
-              className="cursor-pointer text-2xl fill-yellow-300"
-              onClick={() => setDarkMode(!darkMode)}
-            />
-          ) : (
-            <BsMoonStarsFill
-              className="cursor-pointer text-2xl  "
-              onClick={() => setDarkMode(!darkMode)}
-            />
-          )}
-        </li>
-        <li className="hover:text-teal-500">
+        <li className="hover:text-cyan-500">
           <a href="#about" className="rounded-md">
             About
           </a>
         </li>
-        <li className="hover:text-teal-500">
+        <li className="hover:text-cyan-500">
           <a href="#services" className="rounded-md ">
             Services
           </a>
         </li>
-        <li className="hover:text-teal-500">
+        <li className="hover:text-cyan-500">
           <a href="#skills" className="rounded-md ">
             Skills
+          </a>
+        </li>
+        <li className="hover:text-cyan-500">
+          <a href="#experience" className="rounded-md ">
+            Experiences
           </a>
         </li>
         <li className="hover:text-teal-500">
